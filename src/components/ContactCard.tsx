80e1bc61f3b796f2f62c2d0ee2979323a930cb1f
SixPlusOne7 <163210546+SixPlusOne7@users.noreply.github.com>
2025-11-01 15:37:54 -1000

    list contacts page mockup finished in 10 minutes.

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+/* eslint-disable react/prop-types */
+import { Card, Image } from 'react-bootstrap';
+import { Contact } from '@/lib/validationSchemas';
+
+interface ContactCardProps {
+  contact: Contact;
+}
+
+const ContactCard: React.FC<ContactCardProps> = ({ contact }) => (
+  <Card className="h-100">
+    <Card.Header className="text-center">
+      <Image
+        src={contact.image}
+        alt={`${contact.firstName} ${contact.lastName}`}
+        width={75}
+        roundedCircle
+      />
+      <Card.Title>{`${contact.firstName} ${contact.lastName}`}</Card.Title>
+      <Card.Subtitle className="mb-2 text-muted">{contact.address}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+export default ContactCard;
